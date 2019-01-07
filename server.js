58dfd575f83f0909c0d86dd7a507dbb513ea5036
joshua sorkin <joshua.d.sorkin@gmail.com>
2019-01-06 16:16:46 -0800

    added taskrouter code from TwilioDevEd/task-router-node repo

--- server.js
+++ server.js
@@ -10,16 +10,35 @@ const workspaceSid = process.env.TWILIO_WORKSPACE_SID; //add your workspace sid
 const workflowSid=process.env.TWILIO_WORKFLOW_SID;
 const client=require('twilio')(accountSid,authToken);
 const http_port=1337;
+var workspace = require('./lib/workspace')();
 
 app.post('/enqueue_call',function(req,res){
 	const response=new VoiceResponse();
 	const enqueue=response.enqueue({
-		workflowSid:workflow_sid,
+		workflowSid:workspace.workflowSid,
 		waitUrl:process.env.WAIT_URL
 	});
 	res.send(response.toString());
 });
 
-app.listen(http_port,()=>
-	console.log(`app listening on port ${http_port}`)
-);
+app.listen(http_port,()=>{
+	console.log(`app listening on port ${http_port}`);
+	console.log("Configuring a Twilio's TaskRouter Workspace");
+  workspace.setup().then(function (data) {
+    app.set('workerInfo', data[0]);
+    app.set('workspaceInfo', data[1]);
+    console.log(data)
+    console.log('Application configured!');
+    console.log('Call your Twilio number at: ' + process.env.TWILIO_NUMBER);
+  })
+  .catch(err=>{console.log(err)});
+});
+
+var envVariables = ['TWILIO_ACCOUNT_SID', 'TWILIO_AUTH_TOKEN',
+  'TWILIO_NUMBER', 'MISSED_CALLS_EMAIL_ADDRESS', 'HOST', 'ALICE_NUMBER',
+  'BOB_NUMBER'];
+envVariables.forEach(function (variableName) {
+  if (!process.env[variableName]) {
+    exitErrorHandler(variableName + ' variable is not set on your environment. Please check your environment variables or your .env file from .env.example');
+  }
+});
