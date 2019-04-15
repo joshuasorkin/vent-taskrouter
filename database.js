711c89d485b9d22ee349ec74f1bfc8627952abc2
joshua sorkin <joshua.d.sorkin@gmail.com>
2019-04-15 01:04:40 -0700

    add logging to createAvailableNotificationRequest

--- database.js
+++ database.js
@@ -64,8 +64,9 @@ class Database{
 	}
 
 	async createAvailableNotificationRequest(workerSid){
-		var selectResult=await sequelize.query("select * from worker where sid='"+workerSid+"'");
-		console.log("createAvailableNotificationRequest: selectResult: "+JSON.stringify(selectResult));
+		var selectResult=await sequelize.query("select * from worker where sid='"+workerSid+"'",
+		{ type: sequelize.QueryTypes.SELECT});
+		console.log("createAvailableNotificationRequest: selectResult[0]: "+selectResult[0]);
 		var id=selectResult[0].id;
 		console.log("createAvailableNotificationRequest: id is "+id);
 		var insertResult=await sequelize.query("insert into available_notification_request (worker_id) values ("+id+")");
