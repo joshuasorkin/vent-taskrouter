6ec326392ef737df816ecc3507ebbd0f4a248ee6
joshua sorkin <joshua.d.sorkin@gmail.com>
2019-04-24 22:04:50 -0700

    change worker_test to take a command line parameter

--- worker_test.js
+++ worker_test.js
@@ -8,18 +8,21 @@ const client=require('twilio')(accountSid,authToken);
 
 clientWorkspace=client.taskrouter.workspaces(workspaceSid);
 var workerObj=new Worker(clientWorkspace);
+var args=process.argv.slice(2);
+var workerSid=args[0];
 
 //workerObj.updateWorkerNameFromSid(null,"helloNewWorker");
-async function update(){
+async function update(workerSid){
     var result;
     var workerEntity=await clientWorkspace.workers('WK425caf724515f59b5620fba1af1e1fd8')
     .fetch();
-    workerObj.updateWorkerAddAttributeArrayValue(workerEntity,"do_not_contact","+19257251305");
+    workerObj.updateWorkerAddAttributeArrayValue(workerEntity,"do_not_contact",workerSid);
 }
 
 
+
 //workerObj.createWorker("abcxisiwe","zzyswiew");
-update();
+update(workerSid);
 
 //worker.addAllWorkersToDatabase();
 //workerObj.updateWorkerAddAttribute(null,null,null);
