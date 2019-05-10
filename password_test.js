532e5ad536494e6c6864c6a94838f40d5caf9c6b
joshua sorkin <joshua.d.sorkin@gmail.com>
2019-05-10 01:03:56 -0700

    change variable name in password_test

--- password_test.js
+++ password_test.js
@@ -1,8 +1,8 @@
 const Password=require('./password');
-var password=new Password();
+var passwordObj=new Password();
 
 var args=process.argv.slice(2);
 var workerSid=args[0];
 var password=args[1];
 var adminTask=args[2];
-password.insertPassword(workerSid,password,adminTask);
+passwordObj.insertPassword(workerSid,password,adminTask);
