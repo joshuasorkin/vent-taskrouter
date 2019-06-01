562892db84339add5e3e3a1899e2e10f0c156f6e
joshua sorkin <joshua.d.sorkin@gmail.com>
2019-05-31 21:51:33 -0700

    (this.) scope

--- twimlBuilder.js
+++ twimlBuilder.js
@@ -49,7 +49,7 @@ class TwimlBuilder{
             action:url,
             method:'GET'
         });
-        playChime(gather);
+        this.playChime(gather);
         this.say(gather,'How many minutes would you like?  Enter '+minMinutes+' to '+maxMinutes+', followed by the pound key.');
         this.say(response,"I didn't receive any input.  Good-bye.");
     }
