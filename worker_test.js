871aa1ab2cbed9f7421ff2826cb17212c162f09b
joshua sorkin <joshua.d.sorkin@gmail.com>
2019-06-06 15:27:46 -0700

    changing worker_test to check .list() issue

--- worker_test.js
+++ worker_test.js
@@ -1,10 +1,11 @@
+/*
 require('env2')('.env');
 const Worker=require('./worker');
 const accountSid = process.env.TWILIO_ACCOUNT_SID; //add your account sid
 const authToken = process.env.TWILIO_AUTH_TOKEN; //add your auth token
 const workspaceSid = process.env.TWILIO_WORKSPACE_SID; //add your workspace sid
 const client=require('twilio')(accountSid,authToken);
-
+*/
 
 //clientWorkspace=client.taskrouter.workspaces(workspaceSid);
 
