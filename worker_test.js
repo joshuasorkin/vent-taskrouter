60be23a3f05b2ee9efd92d497dc4242e0a5d1b96
joshua sorkin <joshua.d.sorkin@gmail.com>
2019-06-06 16:03:32 -0700

    re-added require worker

--- worker_test.js
+++ worker_test.js
@@ -1,7 +1,7 @@
 
 require('env2')('.env');
-/*
 const Worker=require('./worker');
+/*
 const accountSid = process.env.TWILIO_ACCOUNT_SID; //add your account sid
 const authToken = process.env.TWILIO_AUTH_TOKEN; //add your auth token
 const workspaceSid = process.env.TWILIO_WORKSPACE_SID; //add your workspace sid
