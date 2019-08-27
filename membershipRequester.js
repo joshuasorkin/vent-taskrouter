9a238822b66f58ea7d80957e9b8597c3d2300058
joshua sorkin <joshua.d.sorkin@gmail.com>
2019-08-27 03:21:52 -0700

    trying to get correct getRndInteger value

--- membershipRequester.js
+++ membershipRequester.js
@@ -92,6 +92,8 @@ class MembershipRequester{
     }
 
     getRndInteger(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
         var rndInteger=Math.floor(Math.random() * (max - min + 1) ) + min;
         console.log("getRndInteger: "+rndInteger);
         return rndInteger;
