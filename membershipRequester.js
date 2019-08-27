bf989463bdddb20fbb561f7f757a2534697e147e
joshua sorkin <joshua.d.sorkin@gmail.com>
2019-08-27 03:17:10 -0700

    logging getRndInteger value

--- membershipRequester.js
+++ membershipRequester.js
@@ -92,7 +92,9 @@ class MembershipRequester{
     }
 
     getRndInteger(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) ) + min;
+        var rndInteger=Math.floor(Math.random() * (max - min + 1) ) + min;
+        console.log("getRndInteger: "+rndInteger);
+        return rndInteger;
     }
 }
 
