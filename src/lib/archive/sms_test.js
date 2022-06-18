7e93d32a91ee0227f8b8247acd8ff302fda5b041
Mina Demian <mina@minademian.com>
2022-06-18 21:39:52 +0200

    chore: all *_test.js files into one dir

---
+++ src/lib/archive/sms_test.js
@@ -0,0 +1,7 @@
+const Sms = require("../sms");
+
+var args = process.argv.slice(2);
+var friendlyName = args[0];
+var contact_uri = args[1];
+var sms = new Sms();
+sms.sendAddNotification(friendlyName, contact_uri);
