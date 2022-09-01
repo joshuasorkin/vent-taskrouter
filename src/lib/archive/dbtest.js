6400194e17e1bd60ac009b8a6fd48a12e2ed22ab
Mina Demian <mina@minademian.com>
2022-09-01 22:16:37 +0200

    style: remove commented out code

--- src/lib/archive/dbtest.js
+++ src/lib/archive/dbtest.js
@@ -1,7 +1,7 @@
 const Database = require("../../config/database");
 require("env2")(".env");
 
-var db = new Database();
+var db = Database.getInstance();
 var args = process.argv.slice(2);
 var workerSid = args[0];
 
@@ -15,54 +15,3 @@ async function insert() {
 }
 
 console.log(insert());
-/*
-worker={
-	contact_uri:'+15105753138',
-	sid:'3i9un9384r8939'
-}
-db.getWorkerSid(worker.contact_uri)
-.then(result=>console.log(result));
-*/
-
-//db.updateWorkerContact_uri("+15105753139","+15105753138")
-/*
-function show(contact_uri){
-	console.log("contact_uri is: "+contact_uri);
-}
-db.iterateThroughUnsentNotificationsForMessaging(show);
-*/
-
-/*
-db.updateNotificationToSent("WK425caf724515f59b5620fba1af1e1fd8")
-.then(result=>{
-	console.log("dbtest: "+JSON.stringify(result));
-	db.updateNotificationToSent("abcxy")
-	.then(result=>{
-		console.log("dbtest: "+result);
-	})
-});
-*/
-/*
-db.createAvailableNotificationRequest("WK425caf724515f59b5620fba1af1e1fd8")
-.then(result=>{
-	if (result==null){
-		console.log("successfully created request");
-	}
-	else{
-		console.log("problem creating request: "+result);
-	}
-	console.log(result);
-	console.log("moving on to next worker...");
-	db.createAvailableNotificationRequest("WK007e94671dc41a73ee8955aacf6ec743");
-});
-*/
-
-/*
-db.getAvailableAgent(process.env.testgoodphonenumber)ge
-.then(res=>{
-	console.log(res[0].toagentphonenumber);
-})
-.catch(err=>{
-	console.log("error: "+err);
-});
-*/
