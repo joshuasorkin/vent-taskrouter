70b396812e2efc26e7dbb4b8607836a7588a93c6
Mina Demian <mina@minademian.com>
2022-10-19 07:52:56 +0200

    chore: remove test file

--- src/other_route.js
+++
@@ -1,8 +0,0 @@
-var express=require('express');
-var router=express.Router();
-
-router.get('/other_route_endpoint',function(req,res){
-    res.send('other_route_endpoint');
-});
-
-module.exports.router=router;
